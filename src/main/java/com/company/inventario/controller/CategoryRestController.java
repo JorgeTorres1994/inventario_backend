d081398f5ca06ea1c18ad0a07b9dd42c6e09c3e8
Jorge Anthony Torres Pastor <34630883+JorgeTorres1994@users.noreply.github.com>
2024-07-03 05:24:52 -0500

    puerto de origen FRONT-END

--- src/main/java/com/company/inventario/controller/CategoryRestController.java
+++ src/main/java/com/company/inventario/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventario.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.company.inventario.model.Category;
 import com.company.inventario.response.CategoryResponseRest;
 import com.company.inventario.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"}) //Puerto de origen para conectar con el FRONT-END
 @RestController
 @RequestMapping("/api/v1") //url del api
 public class CategoryRestController {
